adcc35b16a03f76ca37296f38ba84ac7da4528db
joe_hallinan <43181783+G00267801@users.noreply.github.com>
2020-01-07 14:39:09 +0000

    commit

---
+++ De-Crypt Puzzle Game/Assets/Game Puzzle/Scripts/GameControl2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl2 : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] SLS_Rocket;
+
+    [SerializeField]
+    private GameObject winText;
+
+    public static bool youWin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+        youWin = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (SLS_Rocket[0].rotation.z == 0 &&
+           SLS_Rocket[1].rotation.z == 0 &&
+           SLS_Rocket[2].rotation.z == 0 &&
+            SLS_Rocket[3].rotation.z == 0 &&
+           SLS_Rocket[4].rotation.z == 0 &&
+            SLS_Rocket[5].rotation.z == 0)
+        {
+            youWin = true;
+            winText.SetActive(true);
+        }
+
+    }
+}
+
---
+++ De-Crypt Puzzle Game/Assets/Game Puzzle/Scripts/TouchRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchRotate : MonoBehaviour
+{
+  
+    public void OnMouseDown()
+    {
+        Debug.Log("clicked");
+        if (!GameControl.youWin)
+        {
+            transform.Rotate(0f, 0f, 90f);
+        }
+    }
+}
